6f24b8c20ef1ac135b78ab9459c5ffe404910a84
Mohamed Yousif <mmbusif@gmail.com>
2020-12-19 16:12:43 +0200

    small additions to the dasbhoard

--- src/edit.js
+++ src/edit.js
@@ -23,31 +23,75 @@ export const PostCreate = (props) => (
 );
 
 
+const choices = [
+    {id: 1, name: "vehicle"},
+    {id: 2, name: "ac"},
+    {id: 3, name: "flies"},
+    {id: 4, name: "disinfectant"},
+    {id: 5, name: "sink"},
+    {id: 6, name: "electricity"},
+    {id: 7, name: "delivery"},
+    {id: 8, name: "carpenter"},
+    {id: 9, name: "colors"},
+    {id: 10, name: "ceramics"},
+    {id: 11, name: "smith"},
+    {id: 12, name: "alumnuim"},
+    {id: 13, name: "kitchen"},
+    {id: 14, name: "electronics"},
+    {id: 15, name: "mobile_fix"},
+    {id: 16, name: "cctv"},
+    {id: 17, name: "parties"},
+    {id: 18, name: "safari"},
+    {id: 19, name: "sweets"},
+    {id: 20, name: "family_biz"},
+    {id: 21, name: "taxi"},
+    {id:22, name: "wiw"},
+    {id: 23, name: "barber"},
+    {id: 24, name: "wash"},
+    {id: 25, name: "coop"},
+    {id: 26, name: "vegan"},
+    {id: 27, name: "meat"},
+    {id: 28, name: "coffee"},
+    {id: 29, name: "cook"},
+    {id: 30, name: "pencils"},
+    {id: 31, name: "teachers"},
+    {id: 32, name: "students"},
+    {id: 33, name: "car_ele"},
+    {id: 34, name: "car_mech"},
+    {id: 35, name: "batteries"},
+    {id: 36, name: "car_mech"},
+    {id: 37, name: "long_vehicle"},
+    {id: 38, name: "soccer"},
+    {id: 39, name: "marriage"},
+    {id: 40, name: "car_wash"},
+ ];
+
 export const PostEdit = (props) => (
     <Edit {...props}>
-                <SimpleForm>
-                    
+            <SimpleForm>        
             <TextInput disabled label="Id" source="id" />
             <TextInput source="fullname" />
-            <RichTextInput  source="description" />
+            <RichTextInput source="description" />
             <BooleanInput source="is_provider" />
             <BooleanInput source="is_active"/>
-            <SelectInput helperText="Set the service provider rating" source="score" choices={[
+            <BooleanInput source="mobile_checked"/>
+            
+            <SelectInput multiple helperText="Set the service provider rating" source="score" choices={[
             { id: 1, name: '1' },
             { id: 2, name: '2' },
             { id: 3, name: '3' },
             { id: 4, name: '4' },
             { id: 5, name: '5' },
-
-   
-]} 
-/>
+            
+            ]} 
+            />
 
 
 <ImageInput source="image"/>
+<AutocompleteArrayInput source="services" choices={choices} translateChoice={false}/>
 
         
-<SelectInput source="services" multiple />
+{/* <SelectInput source="services" multiple /> */}
 
 
 </SimpleForm>
